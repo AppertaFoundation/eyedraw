6d8c5d0103f1ee3729dd9e61b1126d340724bba7
Bill Aylward <bill.aylward@openeyes.org.uk>
2012-06-17 18:04:52 +0100

    Added tooltips for doodles in drawing and added new doodles

--- views/OEEyeDrawWidget.php
+++ views/OEEyeDrawWidget.php
@@ -25,6 +25,7 @@
 			<?php }?>
 		</div>
 	<?php }?>
+    <span id="canvasTooltip"></span>
 	<canvas id="<?php echo $canvasId?>" class="<?php if ($isEditable) { echo 'edit'; } else { echo 'display'; }?>" width="<?php echo $size?>" height="<?php echo $size?>" tabindex="1"<?php if ($canvasStyle) {?> style="<?php echo $canvasStyle?>"<?php }?>></canvas>
 	<input type="hidden" id="<?php echo $inputId?>" name="<?php echo $inputName?>" value='<?php echo $this->model[$this->attribute]?>' />
 </div>
