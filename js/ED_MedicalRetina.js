91ee5be2900b7c714cd826e95a2ccec43c11b919
Bill Aylward <bill.aylward@openeyes.org.uk>
2012-12-27 00:51:46 +0000

    Added new fuctions to PostPole doodle

--- js/ED_MedicalRetina.js
+++ js/ED_MedicalRetina.js
@@ -50,6 +50,9 @@ ED.PostPole = function(_drawing, _originX, _originY, _radius, _apexX, _apexY, _s
 	// Set classname
 	this.className = "PostPole";
     
+    // Private parameters
+    this.discRadius = 84;
+    
     // Derived parameters (NB must set a value here to define parameter as a property of the object, even though value set later)
     this.cdRatio = '0';
     
@@ -144,15 +147,14 @@ ED.PostPole.prototype.draw = function(_point)
 	// Call draw method in superclass
 	ED.PostPole.superclass.draw.call(this, _point);
     
-    // Disc radius
-    var rd = 84;
+    // Disc location
     var x = this.drawing.eye == ED.eye.Right?300:-300;
     
 	// Boundary path
 	ctx.beginPath();
     
 	// Optic disc
-	ctx.arc(x, 0, rd, 0, 2 * Math.PI, true);
+	ctx.arc(x, 0, this.discRadius, 0, 2 * Math.PI, true);
     
 	// Set attributes
 	ctx.lineWidth = 4;
@@ -214,7 +216,7 @@ ED.PostPole.prototype.draw = function(_point)
         
         // One disc diameter
         ctx.beginPath();
-        ctx.arc(0, 0, 2 * rd, 2 * Math.PI, 0, false);
+        ctx.arc(0, 0, 2 * this.discRadius, 2 * Math.PI, 0, false);
         ctx.lineWidth = 1;
         ctx.strokeStyle = "gray";
         ctx.stroke();
@@ -249,7 +251,18 @@ ED.PostPole.prototype.description = function()
  */
 ED.PostPole.prototype.isWithinDiscDiametersOfFovea = function(_doodle, _diameters)
 {
-	return (_doodle.originX * _doodle.originX + _doodle.originY * _doodle.originY) < 4 * 84 * 84;
+	return (_doodle.originX * _doodle.originX + _doodle.originY * _doodle.originY) < _diameters * 4 * this.discRadius * this.discRadius;
+}
+
+/**
+ * Tests whether passed doodle is within a the vascular arcades
+ *
+ * @param {Doodle} _doodle The doodle to test
+ * @returns {Bool} True if doodle is within the vascular arcades
+ */
+ED.PostPole.prototype.isWithinArcades = function(_doodle)
+{
+	return (_doodle.originX * _doodle.originX + _doodle.originY * _doodle.originY) < (300 * 300);
 }
 
 /**
@@ -1656,6 +1669,172 @@ ED.LaserSpot.prototype.draw = function(_point)
 ED.LaserSpot.prototype.groupDescription = function()
 {
     return "laser spots";
+}
+
+/**
+ * Sector PRP
+ *
+ * @class SectorPRPPostPole
+ * @property {String} className Name of doodle subclass
+ * @param {Drawing} _drawing
+ * @param {Int} _originX
+ * @param {Int} _originY
+ * @param {Float} _radius
+ * @param {Int} _apexX
+ * @param {Int} _apexY
+ * @param {Float} _scaleX
+ * @param {Float} _scaleY
+ * @param {Float} _arc
+ * @param {Float} _rotation
+ * @param {Int} _order
+ */
+ED.SectorPRPPostPole = function(_drawing, _originX, _originY, _radius, _apexX, _apexY, _scaleX, _scaleY, _arc, _rotation, _order)
+{
+	// Set classname
+	this.className = "SectorPRPPostPole";
+    
+	// Call super-class constructor
+	ED.Doodle.call(this, _drawing, _originX, _originY, _radius, _apexX, _apexY, _scaleX, _scaleY, _arc, _rotation, _order);
+}
+
+/**
+ * Sets superclass and constructor
+ */
+ED.SectorPRPPostPole.prototype = new ED.Doodle;
+ED.SectorPRPPostPole.prototype.constructor = ED.SectorPRPPostPole;
+ED.SectorPRPPostPole.superclass = ED.Doodle.prototype;
+
+/**
+ * Sets handle attributes
+ */
+ED.SectorPRPPostPole.prototype.setHandles = function()
+{
+	this.handleArray[0] = new ED.Handle(null, true, ED.Mode.Arc, false);
+	this.handleArray[3] = new ED.Handle(null, true, ED.Mode.Arc, false);
+}
+
+/**
+ * Set default properties
+ */
+ED.SectorPRPPostPole.prototype.setPropertyDefaults = function()
+{
+	this.isMoveable = false;
+    
+    // Update component of validation array for simple parameters
+    this.parameterValidationArray['arc']['range'].setMinAndMax(Math.PI/6, Math.PI * 2);
+    this.parameterValidationArray['apexX']['range'].setMinAndMax(-0, +0);
+    this.parameterValidationArray['apexY']['range'].setMinAndMax(-400, -100);
+}
+
+/**
+ * Sets default parameters (Only called for new doodles)
+ * Use the setParameter function for derived parameters, as this will also update dependent variables
+ */
+ED.SectorPRPPostPole.prototype.setParameterDefaults = function()
+{
+    this.arc = 55 * Math.PI/180;
+    this.apexY = -300;
+    
+    var doodle = this.drawing.lastDoodleOfClass(this.className);
+    if (doodle)
+    {
+        this.rotation = doodle.rotation + (this.drawing.eye == ED.eye.Right?-1:1) * (doodle.arc/2 + this.arc/2 + Math.PI/12);
+    }
+    else
+    {
+        this.rotation = (this.drawing.eye == ED.eye.Right?-1:1) * this.arc/2;
+    }
+}
+
+/**
+ * Draws doodle or performs a hit test if a Point parameter is passed
+ *
+ * @param {Point} _point Optional point in canvas plane, passed if performing hit test
+ */
+ED.SectorPRPPostPole.prototype.draw = function(_point)
+{
+	// Get context
+	var ctx = this.drawing.context;
+	
+	// Call draw method in superclass
+	ED.SectorPRPPostPole.superclass.draw.call(this, _point);
+    
+	// Radius of outer curve just inside ora on right and left fundus diagrams
+	var ro = 1000/2;
+    var ri = -this.apexY;
+    var r = ri + (ro - ri)/2;
+	
+	// Calculate parameters for arcs
+	var theta = this.arc/2;
+	var arcStart = - Math.PI/2 + theta;
+	var arcEnd = - Math.PI/2 - theta;
+    
+    // Coordinates of 'corners' of SectorPRPPostPole
+	var topRightX = r * Math.sin(theta);
+	var topRightY = - r * Math.cos(theta);
+	var topLeftX = - r * Math.sin(theta);
+	var topLeftY = topRightY;
+    
+	// Boundary path
+	ctx.beginPath();
+    
+	// Arc across to mirror image point on the other side
+	ctx.arc(0, 0, ro, arcStart, arcEnd, true);
+    
+	// Arc back to mirror image point on the other side
+	ctx.arc(0, 0, ri, arcEnd, arcStart, false);
+    
+	// Close path
+	ctx.closePath();
+	
+	// Set line attributes
+	ctx.lineWidth = 40;
+	ctx.fillStyle = "rgba(255,255,255,0)";
+	ctx.strokeStyle = "rgba(255,255,255,0)";
+	
+	// Draw boundary path (also hit testing)
+	this.drawBoundary(_point);
+	
+	// Non boundary drawing
+	if (this.drawFunctionMode == ED.drawFunctionMode.Draw)
+	{
+        // PRP spot data
+        var si = 30;
+        var sd = (30 + si);
+        
+        // Array of number of spots for each radius value
+        var count = [47,41,35,28,22,15];
+        
+        // Iterate through radius and angle to draw sector
+        var i = 0;
+        for (var r = ro - si; r > ri; r -= sd)
+        {
+            var j = 0;
+            
+            for (var a = -Math.PI/2 - arcStart; a < this.arc - Math.PI/2 - arcStart; a += sd/r )
+            {
+                a = -Math.PI/2 - arcStart + j * 2 * Math.PI/count[i];
+                
+                var p = new ED.Point(0,0);
+                p.setWithPolars(r, a);
+                this.drawLaserSpot(ctx, p.x, p.y);
+                
+                j++;
+            }
+            
+            i++;
+        }
+	}
+    
+	// Coordinates of handles (in canvas plane)
+	this.handleArray[0].location = this.transform.transformPoint(new ED.Point(topLeftX, topLeftY));
+	this.handleArray[3].location = this.transform.transformPoint(new ED.Point(topRightX, topRightY));
+	
+	// Draw handles if selected
+	if (this.isSelected && !this.isForDrawing) this.drawHandles(_point);
+    
+	// Return value indicating successful hit test
+	return this.isClicked;
 }
 
 /**
