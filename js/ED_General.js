b57d2de88abdae85df315e132098c0bb8f7e60bd
Bill Aylward <bill.aylward@openeyes.org.uk>
2013-02-18 13:21:14 +0000

    Implemented MouseTest doodle (in ED_General.js) to test mouse position

--- js/ED_General.js
+++ js/ED_General.js
@@ -601,3 +601,88 @@ ED.Label.prototype.description = function()
     return "Peripheral iridectomy at " + this.clockHour() + " o'clock";
 }
 
+/**
+ *  OperatingTable
+ *
+ * @class  MouseTest
+ * @property {String} className Name of doodle subclass
+ * @param {Drawing} _drawing
+ * @param {Int} _originX
+ * @param {Int} _originY
+ * @param {Float} _radius
+ * @param {Int} _apexX
+ * @param {Int} _apexY
+ * @param {Float} _scaleX
+ * @param {Float} _scaleY
+ * @param {Float} _arc
+ * @param {Float} _rotation
+ * @param {Int} _order
+ */
+ED.MouseTest = function(_drawing, _originX, _originY, _radius, _apexX, _apexY, _scaleX, _scaleY, _arc, _rotation, _order)
+{
+	// Set classname
+	this.className = "MouseTest";
+    
+	// Call superclass constructor
+	ED.Doodle.call(this, _drawing, _originX, _originY, _radius, _apexX, _apexY, _scaleX, _scaleY, _arc, _rotation, _order);
+}
+
+/**
+ * Sets superclass and constructor
+ */
+ED.MouseTest.prototype = new ED.Doodle;
+ED.MouseTest.prototype.constructor = ED.MouseTest;
+ED.MouseTest.superclass = ED.Doodle.prototype;
+
+/**
+ * Sets default properties
+ */
+ED.MouseTest.prototype.setPropertyDefaults = function()
+{
+	this.isMoveable = false;
+}
+
+/**
+ * Draws doodle or performs a hit test if a Point parameter is passed
+ *
+ * @param {Point} _point Optional point in canvas plane, passed if performing hit test
+ */
+ED.MouseTest.prototype.draw = function(_point)
+{
+    //if (_point) console.log(_point.x, _point.y);
+    
+	// Get context
+	var ctx = this.drawing.context;
+	
+	// Call draw method in superclass
+	ED.MouseTest.superclass.draw.call(this, _point);
+	
+	// Boundary path
+	ctx.beginPath();
+	
+	// Square
+    var width = 200;
+	ctx.rect(-width/2, -width/2, width, width);
+	
+	// Close path
+	ctx.closePath();
+	
+	// Set line attributes
+	ctx.lineWidth = 1;
+    ctx.fillStyle = "white"
+	ctx.strokeStyle = "blue";
+    
+	// Draw boundary path (also hit testing)
+	this.drawBoundary(_point);
+    
+    if (this.isClicked) console.log(_point.x, _point.y);
+	
+	// Non boundary paths here
+	if (this.drawFunctionMode == ED.drawFunctionMode.Draw)
+	{
+	}
+    
+	// Return value indicating successful hittest
+	return this.isClicked;
+}
+
