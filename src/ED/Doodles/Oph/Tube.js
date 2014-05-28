b67ae27ab29ae1e7adf824660f5bbc07bc87bdde
Jamie Neil <jamie.neil@openeyes.org.uk>
2014-05-28 14:47:48 +0100

    Update reference to doodle handle

--- src/ED/Doodles/Oph/Tube.js
+++ src/ED/Doodles/Oph/Tube.js
@@ -56,7 +56,7 @@ ED.Tube.superclass = ED.Doodle.prototype;
  * Sets handle attributes
  */
 ED.Tube.prototype.setHandles = function() {
-	this.handleArray[4] = new ED.Handle(null, true, ED.Mode.Apex, false);
+	this.handleArray[4] = new ED.Doodle.Handle(null, true, ED.Mode.Apex, false);
 }
 
 /**
