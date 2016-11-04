ea04d25c198bbed22993af628c200029f7ab8dd3
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-11-04 18:23:33 +0000

    + bugfix for checking a match with previous values in the report for autoreporting.

--- src/ED/Controller.js
+++ src/ED/Controller.js
@@ -477,11 +477,14 @@ ED.Controller = (function() {
 	Controller.prototype.autoReport = function(outputElement) {
 		var report = this.drawing.report();
 		if(report){
+
 			report = report.replace(/, /g,"\n");
 			var output = '';
 			var existing = outputElement.value;
 
-			if(existing.match(report)){
+			var reportRegex = String(report).replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+
+			if(existing.match(reportRegex)){
 				outputElement.rows = (existing.match(/\n/g) || []).length + 1;
 				this.previousReport = report;
 				return;
