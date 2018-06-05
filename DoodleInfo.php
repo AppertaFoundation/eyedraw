bbfb613074c6ef04ce85a5dd8d3c49f9dc556d79
Sabi Potondi <sabi.potondi@abehr.com>
2018-06-05 08:49:42 +0000

    Cypass - adding description to DoodleInfo.php

--- DoodleInfo.php
+++ DoodleInfo.php
@@ -81,6 +81,7 @@ class DoodleInfo
         'CottonWoolSpot' => 'Cotton wool spot',
         'Cryo' => 'Cryotherapy scar',
         'CutterPI' => 'Cutter iridectomy',
+        'Cypass' => 'Cypass Stent Insertion',
         'CystoidMacularOedema' => 'Cystoid macular oedema',
         'DendriticUlcer' => 'Dendritic ulcer',
         'DiabeticNV' => 'Diabetic new vessels',
