947dc2ad44d16027b518ceeb04655e98c1ede543
Peter Toth <peter.toth@abehr.com>
2019-04-17 09:49:19 +0100

    OE-7110 change the iris colour (RGB values) in Gonioscopy Angle Grade doodles to match Anterior Segment's

--- src/ED/Doodles/Oph/AngleGradeNorth.js
+++ src/ED/Doodles/Oph/AngleGradeNorth.js
@@ -230,16 +230,16 @@ ED.AngleGradeNorth.prototype.draw = function(_point) {
 	// Set fill attributes (same colour as Iris)
 	switch (this.colour) {
 		case 'Blue':
-			ctx.fillStyle = "rgba(100, 200, 250, 1.0)";
+			ctx.fillStyle = "rgba(160, 221, 251, 1)";
 			break;
 		case 'Brown':
-			ctx.fillStyle = "rgba(172, 100, 55, 1.0)";
+			ctx.fillStyle = "rgba(203, 161, 134, 1)";
 			break;
 		case 'Gray':
-			ctx.fillStyle = "rgba(125, 132, 116, 1.0)";
+			ctx.fillStyle = "rgba(177, 181, 172, 1)";
 			break;
 		case 'Green':
-			ctx.fillStyle = "rgba(114, 172, 62, 1.0)";
+			ctx.fillStyle = "rgba(169, 206, 141, 1)";
 			break;
 	}
 	ctx.strokeStyle = "rgba(100, 100, 100, 1.0)";
