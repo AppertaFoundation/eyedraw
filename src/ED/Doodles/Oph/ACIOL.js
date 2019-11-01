79531e6732e8fe8a58108b134098d5959a3db48f
harikulendran <hari.kulendran@gmail.com>
2019-11-01 14:41:21 +0000

    OE-8733 Added unique group classes to lenses in eyedraw.
    Specified a unique group name, which indicates that any time a doodle
    with this unique group is present in an drawing, it wants to be the only
    doodle of that type in the drawing. By itself this change does not
    enforce this property, it is enforced in OpenEyes branch fix/OE-8733.

--- src/ED/Doodles/Oph/ACIOL.js
+++ src/ED/Doodles/Oph/ACIOL.js
@@ -25,6 +25,7 @@
 ED.ACIOL = function(_drawing, _parameterJSON) {
 	// Set classname
 	this.className = "ACIOL";
+  this.classGroupUnique = "Lens";
     this.csOriginX = -140;
 
 	// Saved parameters
