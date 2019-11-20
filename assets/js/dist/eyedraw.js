0d96272bcb607ffe85b6143ad093ad263e9d8ebe
Szabolcs Potondi <szabolcs.potondi@gmail.com>
2019-11-20 19:59:20 +0000

    OE-9302 - adding Foeva doodle

--- assets/js/dist/eyedraw.js
+++ assets/js/dist/eyedraw.js
@@ -33979,6 +33979,260 @@ ED.FocalLaser.prototype.groupDescription = function() {
  */
 
 /**
+ * Fovea
+ *
+ * @class Fovea
+ * @property {String} className Name of doodle subclass
+ * @param {Drawing} _drawing
+ * @param {Object} _parameterJSON
+ */
+ED.Fovea = function(_drawing, _parameterJSON) {
+	// Set classname
+	this.className = "Fovea";
+
+	this.type = 'Normal fovea';
+	this.subfoveal = 'Type1';
+
+	// Saved parameters
+	this.savedParameterArray = ['originX', 'originY', 'scaleX', 'scaleY', 'type'];
+
+	this.controlParameterArray = {
+		'type': 'Type',
+		'subfoveal': 'Subfoveal'
+	};
+
+	// Call superclass constructor
+	ED.Doodle.call(this, _drawing, _parameterJSON);
+};
+
+/**
+ * Sets superclass and constructor
+ */
+ED.Fovea.prototype = new ED.Doodle;
+ED.Fovea.prototype.constructor = ED.Fovea;
+ED.Fovea.superclass = ED.Doodle.prototype;
+
+/**
+ * Sets handle attributes
+ */
+ED.Fovea.prototype.setHandles = function() {
+	this.handleArray[2] = new ED.Doodle.Handle(null, true, ED.Mode.Scale, false);
+};
+
+/**
+ * Set default properties
+ */
+ED.Fovea.prototype.setPropertyDefaults = function() {
+	this.isRotatable = false;
+
+	// Update component of validation array for simple parameters
+	this.parameterValidationArray['scaleX']['range'].setMinAndMax(+0.5, +2);
+	this.parameterValidationArray['scaleY']['range'].setMinAndMax(+0.5, +2);
+
+	this.parameterValidationArray['type'] = {
+		kind: 'other',
+		type: 'string',
+		list: ['Normal fovea', 'CNV', 'Disciform scar'],
+		animate: false
+	};
+
+	this.parameterValidationArray['subfoveal'] = {
+		kind: 'other',
+		type: 'string',
+		list: ['Type1', 'Type2', 'Type3'],
+		animate: false
+	};
+
+
+};
+
+/**
+ * Sets default parameters
+ */
+ED.Fovea.prototype.setParameterDefaults = function() {
+	this.setOriginWithDisplacements(0, -100);
+	this.setParameterFromString('type', 'Normal fovea');
+	this.setParameterFromString('subfoveal', 'Type1');
+};
+
+/**
+ * Draws doodle or performs a hit test if a Point parameter is passed
+ *
+ * @param {Point} _point Optional point in canvas plane, passed if performing hit test
+ */
+ED.Fovea.prototype.draw = function(_point) {
+	// Get context
+	var ctx = this.drawing.context;
+
+	// Call draw method in superclass
+	ED.Fovea.superclass.draw.call(this, _point);
+
+	if (this.type === 'Normal fovea') {
+		this.drawNormalFovea(ctx, _point);
+	} else if (this.type === 'CNV') {
+		this.drawCNV(ctx, _point, false);
+	} else if(this.type === 'Disciform scar') {
+		this.drawCNV(ctx, _point, true);
+	}
+
+	// Return value indicating successful hit test
+	return this.isClicked;
+};
+
+ED.Fovea.prototype.drawNormalFovea = function(ctx, _point) {
+
+	var r = 28;
+
+	// Blue centre
+	ctx.beginPath();
+	ctx.arc(0, 0, r * 0.8, 0, 2 * Math.PI, false);
+	ctx.lineWidth = 3;
+	ctx.strokeStyle="rgb(89,115,175)";
+	ctx.stroke();
+	ctx.closePath();
+	ctx.fillStyle = "rgb(105,165,234)";
+	ctx.fill();
+
+	this.drawBoundary(_point);
+};
+
+ED.Fovea.prototype.drawCNV = function(ctx, _point, isDisciform) {
+
+	// Boundary path
+	ctx.beginPath();
+
+	// Radius of Fovea
+	var r = 80;
+
+	// Parameters of random curve
+	var n = 16;
+	var phi = 2 * Math.PI / n;
+	var th = 0.5 * Math.PI / n;
+	var b = 4;
+	var point = new ED.Point(0, 0);
+
+	// First point
+	var fp = new ED.Point(0, 0);
+	fp.setWithPolars(r, 0);
+	ctx.moveTo(fp.x, fp.y);
+	var rl = r;
+
+	// Subsequent points
+	for (var i = 0; i < n; i++) {
+		// Get radius of next point
+		var rn = r * (b + ED.randomArray[i]) / b;
+
+		// Control point 1
+		var cp1 = new ED.Point(0, 0);
+		cp1.setWithPolars(rl, i * phi + th);
+
+		// Control point 2
+		var cp2 = new ED.Point(0, 0);
+		cp2.setWithPolars(rn, (i + 1) * phi - th);
+
+		// Next point
+		var pn = new ED.Point(0, 0);
+		pn.setWithPolars(rn, (i + 1) * phi);
+
+		// Assign next point
+		rl = rn;
+
+		// Next point
+		if (i === n - 1) {
+			ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, fp.x, fp.y);
+		} else {
+			ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, pn.x, pn.y);
+		}
+
+		// Control handle point
+		if (i === 1) {
+			point.x = pn.x;
+			point.y = pn.y;
+		}
+	}
+
+	// Close path
+	ctx.closePath();
+
+	// Set attributes
+	ctx.lineWidth = 0;
+	ctx.fillStyle = "red";
+	ctx.strokeStyle = "red";
+
+	// Draw boundary path (also hit testing)
+	this.drawBoundary(_point);
+
+	// Other paths and drawing here
+	if (this.drawFunctionMode == ED.drawFunctionMode.Draw) {
+		// Yellow/Grey centre
+		ctx.beginPath();
+		ctx.arc(0, 0, r * 0.8, 0, 2 * Math.PI, false);
+		ctx.closePath();
+		if (isDisciform) {
+			ctx.fillStyle = "rgb(155,155,155)";
+		} else {
+			ctx.fillStyle = "rgba(255,255,190,1)";
+		}
+		ctx.fill();
+	}
+
+	// Coordinates of handles (in canvas plane)
+	this.handleArray[2].location = this.transform.transformPoint(point);
+
+	// Draw handles if selected
+	if (this.isSelected && !this.isForDrawing) this.drawHandles(_point);
+};
+
+/**
+ * Returns a string containing a text description of the doodle
+ *
+ * @returns {String} Description of doodle
+ */
+ED.Fovea.prototype.description = function() {
+
+	if (this.type === 'CNV') {
+		return 'Choroidal neovascular membrane (' + this.subfoveal + ')';
+	} else if (this.type === 'Disciform scar') {
+		return 'Disciform scar';
+	}
+	return "Normal fovea";
+};
+
+/**
+ * Returns the SnoMed code of the doodle
+ *
+ * @returns {Int} SnoMed code of entity representated by doodle
+ */
+ED.Fovea.prototype.snomedCode = function() {
+
+	if (this.type === 'Normal fovea') {
+		return 67046006;
+	} else if (this.type === 'CNV') {
+		return 75971007;
+	}
+
+	// Disciform scar
+	return 414173003;
+};
+
+
+/**
+ * OpenEyes
+ *
+ * Copyright (C) OpenEyes Foundation, 2011-2017
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright 2011-2017, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+
+/**
  * Fuch's endothelial Dystrophy
  *
  * @class Fuchs
