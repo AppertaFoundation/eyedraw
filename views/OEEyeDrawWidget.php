87c048c1a90aeaec59763fb9a7786161932e6639
Szabolcs Potondi <szabolcs.potondi@gmail.com>
2020-05-09 13:02:52 +0100

    OE-9952 - fixing view and additional dom elements, event listeners

--- views/OEEyeDrawWidget.php
+++ views/OEEyeDrawWidget.php
@@ -15,66 +15,16 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
-<!-- *******  ED Inline Widget ******* -->
-<div class="ed2-widget <?php echo ($isEditable) ? ' edit' : ' display';?>" id="eyedrawwidget_<?php echo $idSuffix ?>">
-        <?php if ("toolbar") :?>
-            <div class="ed2-toolbar"><?php $this->render('toolbar', $data);?></div>
-        <?php endif; ?>
-
-        <!-- Select Doodles -->
-<!--		<div class="ed2-toolbar">-->
-<!--			--><?php //if (@$data['imageUrl']) {?>
-<!--				<img src="--><?php //echo $data['imageUrl']?><!--" height=--><?//=$height?><!-- width=--><?//=$width?><!-- />-->
-<!--			--><?php //}else{?>
-<!--            <div class="ed2-editor-wrap">-->
-<!--				--><?php //$this->render('editor', $data);?>
-<!--                --><?php //$this->render('fields', $data);?>
-<!--            </div>-->
-<!--			--><?php //}?>
-<!--		</div>-->
-
-        <div class="ed2-body">
-            <!-- SELECTED DOODLE -->
-            <?php if ($showDrawingControls && $mode !== 'view') {?>
-                <div class="ed2-drawing-controls flex-layout<?= $toggleScale ? ' ed2-feature-zoom': '';?>">
-                    <!-- DRAWING TOOLBAR -->
-                    <ul class="ed2-toolbar-panel ed2-drawing-toolbar">
-                        <li>
-                            <a class="ed-button" href="#" data-function="resetEyedraw">
-                                <i class="icon-ed-reset pad"></i>
-                                <span class="label">reset pad</span>
-                            </a>
-                        </li>
-                        <?php if ($toggleScale) {?>
-                            <li>
-                                <a class="ed-button" href="#" data-function="toggleZoom">
-                                    <i class="icon-ed-zoom-out pad"></i>
-                                    <span class="label">zoom-out pad</span>
-                                </a>
-                            </li>
-                        <?php }?>
-                    </ul>
-                    <div class="ed2-selected-doodle">
-                        <select class="ed2-selected-doodle-select cols-full" id="ed_example_selected_doodle">
-                        </select>
-                    </div>
-                    <div class="ed2-search">
-                        <input type="text" class="search" id="js-idg-demo-ed2-search-input">
-                        <!-- note: this can be completely JS generated if you want -->
-                        <ul class="oe-autocomplete" style="display: none;">
-                            <li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li><li><a>No Evidence of Glaucoma</a></li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div class="ed2-no-doodle-elements">
-                    <ul class="no-doodles">
-                        <li>No Evidence of Glaucoma<i class="oe-i remove-circle small-icon pad-left"></i></li>
-                        <li>Glaucoma Suspect<i class="oe-i remove-circle small-icon pad-left"></i></li>
-                        <li class="has-options">Has options (like a doodle)</li>
-                    </ul>
-                </div>
+<div class="EyeDrawWidget ed-widget <?php echo ($isEditable) ? ' edit' : ' display';?>" id="eyedrawwidget_<?php echo $idSuffix ?>">
+    <?php $this->render('toolbar', $data);?>
+    <div class="ed-body">
+        <div class="ed-editor-container">
+            <?php if (@$data['imageUrl']) {?>
+                <img src="<?php echo $data['imageUrl']?>" height=<?=$height?> width=<?=$width?> />
+            <?php }else{?>
+                <?php $this->render('editor', $data);?>
             <?php }?>
         </div>
-
+        <?php $this->render('fields', $data);?>
+    </div>
 </div>
