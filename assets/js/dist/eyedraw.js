caf1509b4bf236a4e011b3b345670ead541c57c1
AdrianKamulegeya <adriankamulegeya@gmail.com>
2021-05-18 08:58:01 +0100

    OE-11280 use htmlDecode function for &lt; issue (#253)
    Co-authored-by: @=AdrianKamulegeya <@=adrian.kamulegeya@gmail.com>

--- assets/js/dist/eyedraw.js
+++ assets/js/dist/eyedraw.js
@@ -2916,7 +2916,7 @@ ED.Drawing.prototype.reportData = function() {
 	var grouped = {};
 	for (var i = 0; i < this.doodleArray.length; i++) {
 		var doodle = this.doodleArray[i];
-		var description = doodle.description();
+		var description = OpenEyes.Util.htmlDecode(doodle.description());
 
 		if (doodle.willReport) {
 			var groupDescription = doodle.groupDescription();
